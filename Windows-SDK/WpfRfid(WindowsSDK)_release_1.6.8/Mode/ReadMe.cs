b76c8c59b6c65c24a8c6faf5b24afebf5fe296fa
施探宇 <alecksty@163.com>
2020-05-18 08:58:43 +0800

    updae

---
+++ Windows-SDK/WpfRfid(WindowsSDK)_release_1.6.8/Mode/ReadMe.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace WpfRfid.Mode
+{
+    static class ReadMe
+    {
+        /// <summary>
+        /// 说明
+        /// </summary>
+        private static List<string> notes = new List<string>();
+
+        static ReadMe()
+        {
+            notes.Add("1.使用ListView，替代DataGrid！");
+            notes.Add("2.添加Config代码！");
+        }
+    }
+}
---
+++ Windows-SDK/WpfRfid(WindowsSDK)_release_1.6.9/c.bat
@@ -0,0 +1,94 @@
+del *.rar
+del *.class /s
+del *.apk /s
+del cache.* /s
+del *.bak /s
+del *.tmp /s
+del *.ap_ /s
+del *.bin /s
+del *.dex /s
+del *.lock /s
+
+del *.pdb /s
+del *.ipch /s
+rem del *.winmd /s
+del *.Assemblyinfo.cs /s
+del *.CopyComplete /s
+del *.diagsession /s
+
+del *.ilpdb /s
+del *.ildll /s
+del *.ilexe /s
+del *.mdilexe /s
+del *.mdilpdb /s
+del *.rsp /s
+del *.csv /s
+del *.rsrc /s
+del *.index.txt /s
+del *.tlog /s
+rem del *.dll /s
+rem del *.exe /s
+del *.backup /s
+
+del *.cache /s
+del *.mdb /s
+del *.stamp /s
+del *.resw /s
+del *.xbf /s
+rem del *.g.cs /s
+rem del *.g.i.cs /s
+del *.state /s
+del *.resfiles /s
+del *.intermediate /s
+
+del classes*.jar /s
+del instant-run.* /s
+del instant-run*.jar /s
+del gradle-wrapper.jar /s
+del mockable-android-*.jar /s
+del debug*.jar /s
+del support-*.jar /s
+del internal*.jar /s
+
+del gc.properties /s
+del build_*.ser /s
+
+del *.rawproto /s
+del *.log /s
+del *.json /s
+del *.gson /s
+del *.check_cache /s
+del dex-renamer-state.txt /s
+del *.flat /s
+
+del *.command.txt /s
+del *.stderr.txt /s
+del *.stdout.txt /s
+
+del ndkBuild_build_command.txt /s
+del ndkBuild_build_output.txt /s
+
+del *.o /s
+del *.d /s
+
+del *.pdb /s
+
+del *_jar.xml /s
+del *_aar.xml /s
+
+del ?.jar /s
+del ??.jar /s
+del ???.jar /s
+
+dir *.jar /s
+rem dir *.jar /s >jars.txt
+del "*=" /s
+
+del "*.g.cs" /s
+del "*.g.i.cs" /s
+del "*.baml" /s
+del "*.g.resources" /s
+del "*_MarkupCompile.lref" /s
+del "*_MarkupCompile.i.lref" /s
+
+
