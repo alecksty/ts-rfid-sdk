4f7f5da91d057802ecee73d3e5a56d2815e4e580
施探宇 <alecksty@163.com>
2020-05-23 10:38:25 +0800

    update

---
+++ Windows-SDK/WpfRfid(WindowsSDK)_release_1.7.0/Mode/R.cs
@@ -0,0 +1,99 @@
+namespace WpfRfid.Mode
+{
+    /// <summary>
+    /// 资源文件 - 字符串
+    /// </summary>
+    public static class R
+    {
+        public static string string_str_config_auto => "自动读取";
+        public static string string_str_setup_buffer => "缓存设置";
+        public static string string_str_setup_buffer_start => "开始";
+        public static string string_str_setup_buffer_stop => "停止";
+        public static string string_str_setup_buffer_clean => "清除";
+        public static string string_str_setup_buffer_test => "测试";
+        public static string string_str_post_config => "推送";
+        public static string string_str_post_url => "推送地址";
+        public static string string_str_setup_region_cn800 => "中国800";
+        public static string string_str_setup_region_cn900 => "中国900";
+        public static string string_str_setup_region_usa => "美国";
+        public static string string_str_setup_region_europe => "欧洲";
+        public static string string_str_setup_region_korean => "韩国";
+        public static string string_str_setup_sort_a2z => "正序";
+        public static string string_str_setup_sort_z2a => "倒序";
+        public static string string_str_setup_sort_index => "序号";
+        public static string string_str_setup_sort_epc => "EPC";
+        public static string string_str_setup_sort_signal => "信号值";
+        public static string string_str_setup_sort_time => "时间";
+        public static string string_str_setup_sort_count => "次数";
+        public static string string_str_session_mode_s0 => "S0";
+        public static string string_str_session_mode_s1 => "S1";
+        public static string string_str_session_mode_s2 => "S2";
+        public static string string_str_session_mode_s3 => "S3";
+        public static string string_str_session_mode_sl => "SL";
+        public static string string_str_session_target_a => "目标 A";
+        public static string string_str_session_target_b => "目标 B";
+        public static string string_str_session_target_auto => "自动";
+        public static string string_str_boot_mode_ble => "蓝牙";
+        public static string string_str_boot_mode_wifi => "无线网";
+        public static string string_str_setup_antanna => "天线";
+        public static string string_str_setup_port => "端口";
+        public static string string_str_setup_instruction => "指令";
+        public static string string_str_setup_scan_mode => "模式";
+        public static string string_str_session_onoff => "会话开关";
+        public static string string_str_session_mode => "会话模式";
+        public static string string_str_session_target => "会话目标";
+        public static string string_str_session_q_value => "Q值";
+        public static string string_str_session_scan_time => "扫描时间";
+        public static string string_str_session_invert => "翻转";
+        public static string string_str_session_invert_en => "翻转使能";
+        public static string string_str_session_invert_num => "翻转条件";
+        public static string string_str_setup_channel => "天线";
+        public static string string_str_switch_times => "切换间隔";
+        public static string string_str_setup_scan => "扫描";
+        public static string string_str_setup_per_second => "每秒次数";
+        public static string string_str_setup_inventory => "轮训";
+        public static string string_str_setup_sw_interval => "切换时间";
+        public static string string_str_setup_device => "设备";
+        public static string string_str_setup_config => "配置";
+        public static string string_str_config_power_save => "节能模式";
+        public static string string_str_config_mode => "模式";
+        public static string string_str_config_ssid => "网名";
+        public static string string_str_config_pwd => "密码";
+        public static string string_str_setup_host => "主机";
+        public static string string_str_setup_4g => "4G";
+        public static string string_str_setup_con_type => "承载类型";
+        public static string string_str_setup_apn_name => "APN";
+        public static string string_str_setup_dev_name => "设备名";
+        public static string string_str_setup_action => "动作";
+        public static string string_str_config_read => "读取";
+        public static string string_str_config_write => "写入";
+        public static string string_str_setup_region => "区域";
+        public static string string_str_setup_skip_freq => "跳频";
+        public static string string_str_setup_power => "功率";
+        public static string string_str_setup_operate => "操作";
+        public static string string_str_setup_sort => "排序";
+        public static string string_str_setup_sort_order => "顺序";
+        public static string string_str_setup_sort_title => "字段";
+        public static string string_str_setup_filter => "过滤";
+        public static string string_str_setup_signal => "过滤";
+        public static string string_str_setup_times => "次";
+        public static string string_str_setup_contain => "包含";
+        public static string string_str_setup_tree => "树视图";
+        public static string string_str_setup_root_name => "会话目标";
+        public static string string_str_setup_tree_splite => "根名称";
+        public static string string_str_setup_contain_splite => "分隔符";
+        public static string string_str_setup_display => "显示";
+        public static string string_str_setup_display_invert => "反向显示";
+        public static string string_str_setup_display_text => "显示文字";
+        public static string string_str_setup_display_refresh_count => "刷新数量";
+        public static string string_str_setup_display_refresh_delay => "刷新间隔";
+        public static string string_str_setup_other => "其他";
+        public static string string_str_setup_debug => "调试";
+        public static string string_str_setup_about => "关于";
+        public static string string_str_setup_all => "全部";
+        public static string string_str_no_tag_times => "次无标签";
+        public static string string_str_delay_ms => "毫秒";
+        public static string string_str_delay_times => "次";
+        public static string string_str_power_dbm => "dBm";
+    }
+}
